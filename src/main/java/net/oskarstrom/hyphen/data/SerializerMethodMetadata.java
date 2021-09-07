d786439e3aaf41ed1556f661dd604bba940a645b
alphaqu <24830855+alphaqu@users.noreply.github.com>
2021-09-07 18:44:36 +0200

    some throwable

---
+++ src/main/java/net/oskarstrom/hyphen/data/SerializerMethodMetadata.java
@@ -0,0 +1,18 @@
+package net.oskarstrom.hyphen.data;
+
+import net.oskarstrom.hyphen.ObjectSerializationDef;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SerializerMethodMetadata {
+	public final ClassInfo clazz;
+	public final Map<Field, ObjectSerializationDef> fields;
+
+	public SerializerMethodMetadata(ClassInfo clazz) {
+		this.clazz = clazz;
+		this.fields = new LinkedHashMap<>();
+	}
+}
---
+++ src/main/java/net/oskarstrom/hyphen/util/ThrowHandler.java
@@ -0,0 +1,59 @@
+
+package net.oskarstrom.hyphen.util;
+
+import java.util.function.Function;
+
+public class ThrowHandler {
+
+	//dollar for the styles.
+	public static void fatal(Function<String, RuntimeException> ex, String reason, Throwable... throwable) {
+		StringBuilder builder = new StringBuilder();
+		builder.append('\n');
+		builder.append('\n');
+		builder.append("Reason: ");
+		builder.append("\n\t");
+		builder.append(reason);
+		builder.append('\n');
+		builder.append('\n');
+		builder.append("Detail: ");
+		for (Throwable throwable$ : throwable) {
+			builder.append('\n');
+			for (ThrowEntry entry : throwable$.getEntries()) {
+				builder.append(entry);
+			}
+		}
+		builder.append('\n');
+		builder.append('\n');
+		builder.append("Stacktrace: ");
+		throw ex.apply(builder.toString());
+	}
+
+	public interface Throwable {
+		ThrowEntry[] getEntries();
+	}
+
+	public static class ThrowEntry implements Throwable {
+		public final String key;
+		public final String value;
+
+		public ThrowEntry(String key, String value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public static ThrowEntry of(String key, String value) {
+			return new ThrowEntry(key, value);
+		}
+
+		@Override
+		public ThrowEntry[] getEntries() {
+			return new ThrowEntry[]{this};
+		}
+
+		@Override
+		public String toString() {
+			return "\t" + key + ": " + value;
+		}
+	}
+
+}
