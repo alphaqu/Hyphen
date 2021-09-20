b3af07c3ec024797e1d803566b2217618aff8eb8
alphaqu <24830855+alphaqu@users.noreply.github.com>
2021-09-20 17:31:44 +0200

    IO mode

--- src/main/java/dev/quantumfusion/hyphen/gen/IOMode.java
+++ src/main/java/dev/quantumfusion/hyphen/gen/IOMode.java
@@ -1,7 +1,20 @@
 package dev.quantumfusion.hyphen.gen;
 
 public enum IOMode {
+	/**
+	 * Generate bytecode that accepts the ByteBufferIO directly.
+	 */
 	BYTEBUFFER,
+	/**
+	 * Generate bytecode that accepts the UnsafeIO directly.
+	 */
 	UNSAFE,
-	ARRAY
+	/**
+	 * Generate bytecode that accepts the ArrayIO directly.
+	 */
+	ARRAY,
+	/**
+	 * Generate bytecode that accepts the IOInterface interface. For custom implementations.
+	 */
+	CUSTOM
 }
