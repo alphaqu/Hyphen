790bd23a8e1d9704c00419b4870e5a849a3cccdf
Robbe Pincket <7889478+Kroppeb@users.noreply.github.com>
2021-10-11 19:25:27 +0200

    Fix

--- src/test/java/dev/quantumfusion/hyphen/scan/TestPoly.java
+++ src/test/java/dev/quantumfusion/hyphen/scan/TestPoly.java
@@ -5,10 +5,12 @@ import dev.quantumfusion.hyphen.scan.type.Clazz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class TestPoly {
 	C1<C0> s;
+	C1<C1<C0>> s2;
 
 	@Test
 	void testC1C0() throws NoSuchFieldException {
@@ -23,6 +25,22 @@ public class TestPoly {
 				C3Ignore.class, "C3Ignore<C3Ignore:C = FieldType{C0},C3Ignore:D = FieldType{UNDEFINED}>",
 				C1Pair.class, "C1Pair<C1Pair:A = FieldType{C0}>",
 				RecursiveC.class, "RecursiveC<RecursiveC:T = FieldType{C0}>"
+		));
+	}
+
+	@Test
+	void testC1C1C0() throws NoSuchFieldException {
+		check("s2", linkedMapOf(
+				C1.class, "C1<C1:A = FieldType{C1<C1:A = FieldType{C0}>}>",
+				C2.class, "C2<C2:B = FieldType{C1<C1:A = FieldType{C0}>}>",
+				C3.class, "C3<C3:C = FieldType{C1<C1:A = FieldType{C0}>},C3:D = FieldType{UNDEFINED}>",
+				C3Def.class, "C3Def<C3Def:E = FieldType{C1<C1:A = FieldType{C0}>}>",
+				// CoWrappedC1.class, "CoWrappedC1<CoWrappedC1:CA = FieldType{C1<C1:A = FieldType{C0}>},CoWrappedC1:C1<C1:A = FieldType{C0}>>",
+				// CoWrappedC1Extends.class, "CoWrappedC1Extends<CoWrappedC1Extends:CA = FieldType{C1<C1:A = FieldType{C0}>},CoWrappedC1Extends:A = C1<C1:A = FieldType{C0}>>",
+				// CoWrappedC1Super.class, "CoWrappedC1Super<CoWrappedC1Super:CA = FieldType{C1<C1:A = FieldType{C0}>},CoWrappedC1Super:A = C1<C1:A = FieldType{C0}>>",
+				C3Ignore.class, "C3Ignore<C3Ignore:C = FieldType{C1<C1:A = FieldType{C0}>},C3Ignore:D = FieldType{UNDEFINED}>",
+				C1Pair.class, "C1Pair<C1Pair:A = FieldType{C1<C1:A = FieldType{C0}>}>",
+				RecursiveC.class, "RecursiveC<RecursiveC:T = FieldType{C1<C1:A = FieldType{C0}>}>"
 		));
 	}
 
@@ -50,7 +68,18 @@ public class TestPoly {
 	private static void printAndSupers(Clazz subClazz) {
 		System.out.println(subClazz);
 		Clazz aSuper = subClazz.getSuper();
-		if(aSuper != null)
+		if (aSuper != null)
 			printAndSupers(aSuper);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <K, V> Map<K, V> linkedMapOf(Object... entries) {
+		LinkedHashMap<K, V> map = new LinkedHashMap<>();
+
+		for (int i = 0; i < entries.length; i += 2) {
+			map.put((K)entries[i], (V)entries[i + 1]);
+		}
+
+		return map;
 	}
 }
