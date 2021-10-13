63343d0385605731b78254d8b6c3d558ce6441a2
yanchu0612 <yan.gyunashyan@gmail.com>
2021-10-13 16:11:47 +0200

    Fix marge

--- src/main/java/dev/quantumfusion/hyphen/util/ScanUtil.java
+++ src/main/java/dev/quantumfusion/hyphen/util/ScanUtil.java
@@ -56,27 +56,96 @@ public class ScanUtil {
 	}
 
 	public static AnnotatedType wrap(Type clazz) {
-		return new AnnotatedType() {
-			@Override
-			public Type getType() {
-				return clazz;
-			}
+		if (clazz instanceof ParameterizedType parameterizedType)
+			return new AnnotatedParameterizedTypeWrap(parameterizedType);
+		if (clazz instanceof TypeVariable typeVariable) return new AnnotatedTypeVariableWrap(typeVariable);
+		if (clazz instanceof GenericArrayType genericArrayType) return new AnnotatedArrayTypeWrap(genericArrayType);
+		if (clazz instanceof Class<?> c) if (c.isArray()) return new AnnotatedArrayTypeWrap(c);
 
-			@Override
-			public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
-				return null;
-			}
+		return new AnnotatedWrapped(clazz);
+	}
 
-			@Override
-			public Annotation[] getAnnotations() {
-				return new Annotation[0];
-			}
+	private static class AnnotatedParameterizedTypeWrap extends AnnotatedWrapped implements AnnotatedParameterizedType {
+		private final AnnotatedType[] annotatedTypes;
 
-			@Override
-			public Annotation[] getDeclaredAnnotations() {
-				return new Annotation[0];
+		public AnnotatedParameterizedTypeWrap(ParameterizedType type) {
+			super(type);
+			final Type[] actualTypeArguments = type.getActualTypeArguments();
+			annotatedTypes = new AnnotatedType[actualTypeArguments.length];
+			for (int i = 0; i < actualTypeArguments.length; i++) {
+				annotatedTypes[i] = wrap(actualTypeArguments[i]);
 			}
-		};
+		}
+
+		@Override
+		public AnnotatedType[] getAnnotatedActualTypeArguments() {
+			return annotatedTypes;
+		}
+	}
+
+	private static class AnnotatedArrayTypeWrap extends AnnotatedWrapped implements AnnotatedArrayType {
+		private final AnnotatedType component;
+
+		public AnnotatedArrayTypeWrap(Type type) {
+			super(type);
+			if (type instanceof Class<?> c) {
+				component = wrap(c.getComponentType());
+			} else if (type instanceof GenericArrayType arr) {
+				component = wrap(arr.getGenericComponentType());
+			} else {
+				throw new RuntimeException("what");
+			}
+		}
+
+		@Override
+		public AnnotatedType getAnnotatedGenericComponentType() {
+			return component;
+		}
+	}
+
+	private static class AnnotatedTypeVariableWrap extends AnnotatedWrapped implements AnnotatedTypeVariable {
+		public AnnotatedTypeVariableWrap(TypeVariable<?> type) {
+			super(type);
+		}
+
+		@Override
+		public AnnotatedType[] getAnnotatedBounds() {
+			return ((TypeVariable<?>) type).getAnnotatedBounds();
+		}
+	}
+
+
+	private static class AnnotatedWrapped implements AnnotatedType {
+		protected final Type type;
+
+		public AnnotatedWrapped(Type type) {
+			this.type = type;
+		}
+
+		@Override
+		public Type getType() {
+			return type;
+		}
+
+		@Override
+		public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+			return null;
+		}
+
+		@Override
+		public Annotation[] getAnnotations() {
+			return new Annotation[0];
+		}
+
+		@Override
+		public Annotation[] getDeclaredAnnotations() {
+			return new Annotation[0];
+		}
+
+		@Override
+		public AnnotatedType getAnnotatedOwnerType() {
+			return null;
+		}
 	}
 
 	public static Class<?> getClassFrom(AnnotatedType type) {
@@ -84,8 +153,8 @@ public class ScanUtil {
 	}
 
 	public static Class<?> getClassFrom(Type type) {
-		if(type instanceof Class<?> c) return c;
-		if(type instanceof ParameterizedType pt) return getClassFrom(pt.getRawType());
+		if (type instanceof Class<?> c) return c;
+		if (type instanceof ParameterizedType pt) return getClassFrom(pt.getRawType());
 
 		throw new IllegalArgumentException(type.getClass() + ": " + type);
 	}
