caf2851fdb7a31270f592e383eb313202b0b1f43
yanchu0612 <yan.gyunashyan@gmail.com>
2021-10-16 12:08:58 +0200

    Fix brain (ishland code)

--- src/main/java/dev/quantumfusion/hyphen/util/GenUtil.java
+++ src/main/java/dev/quantumfusion/hyphen/util/GenUtil.java
@@ -27,15 +27,19 @@ public final class GenUtil {
 	}
 
 	private static final char[] HYPHEN_METHOD_BASE_CHARS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_', '$'};
-	private static final int HYPHEN_METHOD_BASE = HYPHEN_METHOD_BASE_CHARS.length;
 
 	public static String hyphenShortMethodName(int methodId) {
-		final int chars = (int) Math.ceil(methodId / (double) HYPHEN_METHOD_BASE);
-		final char[] out = new char[chars];
-		for (int i = 0; i < chars; i++) {
-			out[i] = HYPHEN_METHOD_BASE_CHARS[Math.min(HYPHEN_METHOD_BASE - 1, methodId)];
-			methodId -= HYPHEN_METHOD_BASE;
+		StringBuilder builder = new StringBuilder();
+		if (methodId == 0) builder.append('A');
+		// ishland big brain
+		while(methodId != 0) {
+			builder.append(HYPHEN_METHOD_BASE_CHARS[(methodId & 63)]);
+			methodId >>= 6;
 		}
-		return new String(out);
+		return builder.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(hyphenShortMethodName(0));
 	}
 }
